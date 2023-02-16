7fa51c6a18abe22c215df0de4e2e18e59cf1097e
Vladislav Macíček <48166821+aeraglyx@users.noreply.github.com>
2023-02-16 13:24:29 +0100

    display frame count in timeline

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
 	"author": "Vladislav Macíček (Aeraglyx)",
 	"description": "All kinds of tools",
 	"blender": (3, 4, 0),
-	"version": (0, 1, 20),
+	"version": (0, 1, 21),
 	"location": "Everywhere. Mostly in node editor and 3D viewport.",
 	"doc_url": "https://github.com/aeraglyx/fulcrum",
 	"category": 'User Interface',
@@ -276,7 +276,7 @@ def register():
 	
 	bpy.types.TOPBAR_HT_upper_bar.append(draw_topbar)
 	bpy.types.OUTLINER_HT_header.append(draw_outliner)
-	bpy.types.DOPESHEET_HT_header.prepend(draw_timeline)
+	bpy.types.DOPESHEET_HT_header.append(draw_timeline)
 	# bpy.app.handlers.depsgraph_update_post.append(ax_depsgraph_handler)
 	bpy.app.handlers.load_post.append(set_restart_needed_flag)
 	
