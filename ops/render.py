4dbe16df5be8b3b92cc5eba66f5f5bf525a0a17f
Vladislav Macíček <vladislav.cam24@gmail.com>
2023-04-02 01:09:06 +0200

    stuff

--- ops/render.py
+++ ops/render.py
@@ -523,7 +523,7 @@ class FULCRUM_OT_prepare_for_render(bpy.types.Operator):
 		bpy.context.scene.render.use_persistent_data = True
 		bpy.context.scene.render.use_border = False
 		bpy.context.scene.render.image_settings.file_format = 'PNG'
-		
+		bpy.context.scene.cycles.use_animated_seed = True
 		bpy.ops.file.make_paths_absolute()
 
 		# TODO purge ?
--- ops/utility.py
+++ ops/utility.py
@@ -30,3 +30,19 @@ class FULCRUM_OT_open_blend_dir(bpy.types.Operator):
 		path = bpy.data.filepath
 		subprocess.Popen('explorer /select,"' + path + '"')
 		return {'FINISHED'}
+
+class FULCRUM_OT_apply_onyx_theme(bpy.types.Operator):
+	
+	bl_idname = "fulcrum.apply_onyx_theme"
+	bl_label = "Apply Onyx Theme"
+	bl_description = ""
+	bl_options = {'REGISTER', 'UNDO'}
+
+	def execute(self, context):
+		return {'FINISHED'}
+		# TODO
+		bpy.ops.script.execute_preset(
+			filepath="C:\\Users\\Aeraglyx\\AppData\\Roaming\\Blender Foundation\\Blender\\3.4\\scripts\\presets\\interface_theme\\onyx.xml",
+			menu_idname="USERPREF_MT_interface_theme_presets"
+		)
+		# TODO matcap, backfacing, cavity
