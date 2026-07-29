de1a6f4d08ab9fe486798819779348a1dffc53dc
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-07-29 02:58:01 +0200

    props: ignore bl type errors

--- props.py
+++ props.py
@@ -5,20 +5,20 @@ class fulcrum_props(bpy.types.PropertyGroup):
     dev: bpy.props.BoolProperty(
         name="More Stuff",
         default=False,
-    )
+    ) # type: ignore
     # restart_needed: bpy.props.BoolProperty(
     #     name="Restart Needed", default=False, options={"SKIP_SAVE"}
     # )
     use_node_handler: bpy.props.BoolProperty(
         name="Node Visualizer",
         default=False,
-    )
+    ) # type: ignore
     node_vis_type: bpy.props.EnumProperty(
         name="Visualization Type",
         description="...",
         items=[("UNUSED", "Unused", ""), ("LEVELS", "Levels", "")],
         default="UNUSED",
-    )
+    ) # type: ignore
 
-    result: bpy.props.FloatProperty(default=1.0)
-    confidence: bpy.props.FloatProperty(default=0.5)
+    result: bpy.props.FloatProperty(default=1.0) # type: ignore
+    confidence: bpy.props.FloatProperty(default=0.5) # type: ignore
