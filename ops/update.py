3a6fc4f6598fcf5bc24d8538274d1e49211f3244
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-07-31 01:47:16 +0200

    update the self updating

--- ops/update.py
+++ ops/update.py
@@ -6,7 +6,27 @@ import shutil
 import zipfile
 import requests
 
-from ..functions import get_addon_version
+from ..functions import get_addon_version, get_addon_dir
+
+
+def update_ext_from_gh(extension_path, repo, branch="main"):
+    archive_link = f"https://github.com/{repo}/archive/refs/heads/{branch}.zip"
+    extension_name = repo.split("/")[-1]
+
+    os.makedirs(extension_path, exist_ok=True)
+    shutil.rmtree(extension_path, ignore_errors=True)
+
+    r = requests.get(archive_link)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(extension_path)
+
+    nested_path = os.path.join(extension_path, f"{extension_name}-{branch}")
+    for filename in os.listdir(nested_path):
+        src = os.path.join(nested_path, filename)
+        dst = os.path.join(extension_path, filename)
+        shutil.move(src, dst)
+
+    os.rmdir(nested_path)
 
 
 class FULCRUM_OT_update_fulcrum(bpy.types.Operator):
@@ -22,31 +42,18 @@ class FULCRUM_OT_update_fulcrum(bpy.types.Operator):
         return True
 
     def execute(self, context):
-        archive_link = "https://github.com/aeraglyx/fulcrum/archive/refs/heads/master.zip"
-        extensions_path = bpy.utils.user_resource('EXTENSIONS')
-        fulcrum_path = os.path.join(extensions_path, "fulcrum")
-        nested_path = os.path.join(fulcrum_path, "fulcrum-master")
+        extension_path = get_addon_dir()
+        if os.path.islink(extension_path):
+            self.report({"WARNING"}, f"Addon is symlinked.")
+            return {"CANCELLED"}
 
-        old_version = get_addon_version()
-
-        shutil.rmtree(fulcrum_path, ignore_errors=True)
-
-        r = requests.get(archive_link)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall(fulcrum_path)
-
-        for filename in os.listdir(nested_path):
-            src = os.path.join(nested_path, filename)
-            dst = os.path.join(fulcrum_path, filename)
-            shutil.move(src, dst)
-
-        os.rmdir(nested_path)
-
-        new_version = get_addon_version()
+        version_old = get_addon_version()
+        update_ext_from_gh(extension_path, repo="aeraglyx/fulcrum", branch="master")
+        version_new = get_addon_version()
 
         self.report(
             {"INFO"},
-            f"Updated from {old_version} to {new_version}. Blender restart required.",
+            f"Updated from {version_old} to {version_new}. Blender restart required.",
         )
 
         return {"FINISHED"}
