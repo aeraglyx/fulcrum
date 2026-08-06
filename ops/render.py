4bf87977342c48b4d2e71e460e2b59dbccbbfac1
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-08-07 00:05:40 +0200

    update anim_time_limit op

--- ops/render.py
+++ ops/render.py
@@ -9,62 +9,46 @@ from ..functions import version_up
 class FULCRUM_OT_anim_time_limit(bpy.types.Operator):
     bl_idname = "fulcrum.anim_time_limit"
     bl_label = "Animation Time Limit"
-    bl_description = "Estimate samples so that render takes a certain time"
+    bl_description = "Calculate and set Time Limit in Cycles"
+    bl_options = {"REGISTER", "UNDO"}
     COMPAT_ENGINES = {"CYCLES"}
 
     @classmethod
     def poll(cls, context):
-        engine = context.scene.render.engine
-        version_ok = bpy.app.version[0] >= 3
-        return engine in cls.COMPAT_ENGINES and version_ok
+        if context.scene.render.engine not in cls.COMPAT_ENGINES:
+            cls.poll_message_set("Works only in Cycles")
+            return False
+        return True
 
     time_needed: bpy.props.FloatProperty(
         name="Time",
         description="How much time you want the render to take",
         unit="TIME_ABSOLUTE",
-        step=100,
         min=0,
         default=3600,
         soft_max=86400,
+        step=6000,
     ) # type: ignore
     multiplier: bpy.props.FloatProperty(
         name="Multiplier",
-        description="So there is some margin",
+        description="Decrease for extra margin, increase when using multiple machines",
         soft_min=0.0,
-        default=0.9,
-        soft_max=1.0,
-    ) # type: ignore
-    custom_range: bpy.props.BoolProperty(
-        name="Custom Range",
-        description="Otherwise use number of frames in scene",
-        default=False,
-    ) # type: ignore
-    frames: bpy.props.IntProperty(
-        name="Custom Frame Range",
-        description="Custom number of frames",
-        min=1,
-        default=100,
+        default=1.0,
+        soft_max=10.0,
+        step=5,
     ) # type: ignore
 
     def execute(self, context):
-        if self.custom_range:
-            frames = self.frames
-        else:
-            start = context.scene.frame_start
-            end = context.scene.frame_end
-            step = context.scene.frame_step
-            frames = (end - start + 1) // step
+        start = context.scene.frame_start
+        end = context.scene.frame_end
+        step = context.scene.frame_step
+        frames = (end - start + 1) // step
 
-        time_limit = self.multiplier * self.time_needed / frames
+        time_limit = self.time_needed * self.multiplier / frames
         context.scene.cycles.time_limit = time_limit
 
-        self.report(
-            {"INFO"}, f"Time Limit set to {time_limit:.2f}s per frame"
-        )  # TODO use appropriate units
+        self.report({"INFO"}, f"Time Limit set to {time_limit:.2f}s per frame")
         return {"FINISHED"}
-
-        # TODO deprecate custom frame range?
-        # TODO number of nodes etc. for render farm?
 
     def invoke(self, context, event):
         wm = context.window_manager
@@ -78,13 +62,6 @@ class FULCRUM_OT_anim_time_limit(bpy.types.Operator):
         col = layout.column(align=True)
         col.prop(self, "time_needed")
         col.prop(self, "multiplier")
-
-        heading = layout.column(align=True, heading="Custom Frame Range")
-        row = heading.row(align=True)
-        row.prop(self, "custom_range", text="")
-        sub = row.row(align=True)
-        sub.active = self.custom_range
-        sub.prop(self, "frames", text="")
 
 
 # TODO clear empty slots
