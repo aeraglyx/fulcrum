0ab1572bac10fb8d3fbc27df3e48cc83cea5fd6a
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-08-07 21:38:56 +0200

    ui: refactor the utility panel

--- ui.py
+++ ui.py
@@ -253,7 +253,7 @@ class FULCRUM_PT_optimization(NodePanel, bpy.types.Panel):
         col.label(text=f"Confidence: {props.confidence*100:.0f}%", icon="RNDCURVE")
 
 
-class FULCRUM_PT_utility_node(NodePanel, bpy.types.Panel):
+class FULCRUM_PT_utility(bpy.types.Panel):
     bl_label = "Utility"
 
     def draw(self, context):
@@ -277,6 +277,10 @@ class FULCRUM_PT_utility_node(NodePanel, bpy.types.Panel):
         )
         if sys.platform == "win32":
             col.operator("wm.console_toggle", icon="CONSOLE")
+
+
+class FULCRUM_PT_utility_node(NodePanel, FULCRUM_PT_utility):
+    pass
 
 
 # ---------------- VIEW 3D ----------------
@@ -453,30 +457,8 @@ class FULCRUM_PT_paint(View3DPanel, bpy.types.Panel):
             props.weight = 1.0
 
 
-class FULCRUM_PT_utility_3d(View3DPanel, bpy.types.Panel):
-    bl_label = "Utility"
-
-    def draw(self, context):
-        layout = self.layout
-
-        col = layout.column(align=True)
-        row = col.row(align=True)
-        row.operator("fulcrum.open_blend_file_dir", icon="FILE_BLEND")
-        row.operator("fulcrum.copy_path_to_clipboard", text="", icon="COPYDOWN")
-        col.operator("fulcrum.backup", icon="FILE_BACKUP")
-
-        col = layout.column(align=True)
-        col.operator("fulcrum.open_blender_user_dir", icon="FILE_SCRIPT")
-        col.operator("fulcrum.background_render_string", icon="SCRIPT")
-
-        col = layout.column(align=True)
-        col.operator(
-            "fulcrum.open_addon_preferences",
-            text="Addon Preferences",
-            icon="PREFERENCES",
-        )
-        if sys.platform == "win32":
-            col.operator("wm.console_toggle", icon="CONSOLE")
+class FULCRUM_PT_utility_3d(View3DPanel, FULCRUM_PT_utility):
+    pass
 
 
 # ---------------- CLIP_EDITOR ----------------
